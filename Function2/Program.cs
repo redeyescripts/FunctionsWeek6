b93e8f8472d4b15d27b73059fa4ef8959c639462
Opilane TTHK <opilane@tthk.ee>
2020-10-08 11:26:26 +0300

    first commit

---
+++ Function2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Params
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ParamsSum(5, 5);
+            DisplayString("100");
+        }
+        public static  void ParamsSum(int a, int b)
+        {
+            int result = a + b;
+                Console.WriteLine(result);
+
+        }
+        public static void DisplayString(string someString)
+        {
+            Console.WriteLine(someString);
+        }
+    }
+}
---
+++ FunctionsWeek6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace FunctionsWeek6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloUser();
+            Triip1();
+        }
+
+        public static void HelloUser()
+        {
+            Console.WriteLine("Enter your name:");
+                string userName = Console.ReadLine();
+            Console.WriteLine($"Hi {userName}");
+        }
+
+        public static void Triip1()
+        {
+            Console.WriteLine("Enter your first number:");
+            int a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your second number:");
+            int b = Int32.Parse(Console.ReadLine());
+
+            int result = a + b;
+                Console.WriteLine(result);
+        }
+    }
+}
